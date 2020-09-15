8811e1752267020b9e1202b7ce9f382352f2ac0c
江兆辉 <949363409@qq..com>
2020-09-15 20:31:38 +0800

    测试styleCi是否生效

--- src/Weather.php
+++ src/Weather.php
@@ -29,8 +29,7 @@ class Weather
 
     // 获取天气
     public function getWeather($city, string $type = 'base', string $format = 'json')
-    {
-        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+    {$url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
         if (!\in_array(\strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: ' . $format);
