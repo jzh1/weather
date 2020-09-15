d4f12b2e17b356cdd88139d033af9f058d41b2e4
jzh1 <jzh1@users.noreply.github.com>
2020-09-15 12:31:55 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidArgumentException.php
+++ src/Exceptions/InvalidArgumentException.php
@@ -1,9 +1,16 @@
 <?php
 
+/*
+ * This file is part of the jzh1/weather.
+ *
+ * (c) jzh <949363409@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Jzh1\Weather\Exceptions;
 
 class InvalidArgumentException extends Exception
 {
-
 }
